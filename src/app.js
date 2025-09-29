9ea2dea0bfbe8c661e8472da17be37a02b680a76
Shivam-3110 <mrshivam00022@gmail.com>
2025-09-29 07:54:14 +0530

    Normalize line endings

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 
 const express = require('express')
 const jwt = require('jsonwebtoken')
+const authRoutes = require("./routes/auth.route")
 
+const cookieParser = require("cookie-parser");
 const app = express()
+
+
+app.use(express.json())
+
+app.use('/auth',authRoutes)
 
 
 
