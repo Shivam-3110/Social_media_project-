860793588e3303f0ee0ee4d152f38a7e6e10e154
Shivam-3110 <mrshivam00022@gmail.com>
2025-10-01 11:59:17 +0530

    auth controller ready and bcrypt is ready

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,1 +1,61 @@
 
+const userModel = require("../models/user.model")
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+async function registerController (req , res){
+    const {username , password} = req.body ;
+    const existingUser = await userModel.findOne({
+        username
+    })
+    if(existingUser){
+        return res.status(409).json({
+            message:"username  alreadyexist"
+        })
+    }
+
+    const user = await userModel.create({
+        username,
+        password : await bcrypt.hash(password,10)
+    })
+     const token = jwt.sign({
+        id:user._id
+     },process.env.JWT_SECRET)
+
+     res.cookie('token',token)
+
+
+    res.status(201).json({
+        message:"user created sucessfully",
+        user
+    })
+}
+async function loginController(req,res){
+    const{username, password} = req.body ;
+    const user = await userModel.findOne({
+        username
+    })
+
+    if(!user) {
+        return res.status(400).json({message:"user not found"})
+    }
+    const isPasswordvalid = await bcrypt.compare(password,user.password) ;
+
+    if(!isPasswordvalid){
+        return res.status(400).json({message:"Invalid password"});
+    }
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+     res.cookie("token", token);
+    res.status(200).json({
+        message:"user loggrd in successfully",
+        user:{
+            username:user.username,
+            id:user._id
+        }
+    })
+}
+
+
+module.exports = {
+    registerController ,
+    loginController
+}
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,40 +1,17 @@
 
+const {registerController , loginController} = require("../controllers/auth.controller")
+
+
 const express = require('express')
 
 const router = express.Router()
-const userModel = require("../models/user.model")
+
 const jwt = require("jsonwebtoken")
 
 
 
-
-router.post('/register',async(req ,res)=>{
-    const{username,password} = req.body
-
-    const existingUser = await userModel.findOne({
-        username
-    })
-    if(existingUser){
-        return res.status(409).json({
-            message:"username  alreadyexist"
-        })
-    }
-
-    const user = await userModel.create({
-        username,password
-    })
-     const token = jwt.sign({
-        id:user._id
-     },process.env.JWT_SECRET)
-
-     res.cookie('token',token)
-
-
-    res.status(201).json({
-        message:"user created sucessfully",
-        user
-    })
-})
+router.post('/register',registerController)
+router.post("/login",loginController)
 
 
 
