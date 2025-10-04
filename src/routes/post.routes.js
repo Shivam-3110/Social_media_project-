1306498d4e4c970b92a35de135f37f4340d0730e
Shivam-3110 <mrshivam00022@gmail.com>
2025-10-04 07:48:37 +0530

    ai api added and post controller fix

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -3,8 +3,12 @@ const express = require('express');
 const router = express.Router();
 const userModel = require("../models/user.model")
 const authMiddleware = require("../middlewares/auth.middleware")
+const multer = require("multer");
 
-router.post("/",authMiddleware,createPostController)
+const upload = multer({ storage:multer.memoryStorage()});
+
+
+router.post("/",authMiddleware,upload.single("image"),createPostController)
 
 
 
